9bf08f22686c6c895c07b64d946bd86863f267af
Alejandro Lopez-Lira <alejandroll10@gmail.com>
2024-06-18 19:03:37 -0400

    Update 4c_ResearchVsDMPlots.R

--- 4c_ResearchVsDMPlots.R
+++ 4c_ResearchVsDMPlots.R
@@ -180,3 +180,93 @@ for (cc in exclCorrelations) {
   
 }
 
+
+# Excluding high correlations and tighter returns --------------------------------------------------
+allRhos = readRDS('../Results/PairwiseCorrelationsActualAndMatches.RDS')
+
+exclCorrelations = c(10)  # c(10, 20, 30, 40, 50)
+r_tol_cor <- 0.1
+for (cc in exclCorrelations) {
+  
+  # filter high correlations from candidate returns
+  corCandidateReturns = candidateReturns %>% 
+    left_join(allRhos, 
+              by = c('candSignalname' = 'candidateSignal', 
+                     'actSignal'      = 'actSignal')) %>% 
+    filter(rho <= cc/100) %>% 
+    select(-rho) %>%
+    left_join(czsum %>% transmute(actSignal = signalname, rbar_op = rbar))
+  
+  print('The following actual signals are dropped by the correlation restriction')
+  print(setdiff(unique(candidateReturns$actSignal), unique(corCandidateReturns$actSignal)))
+  
+  # Normalize candidate returns
+  # In-sample means
+  tempsumCand = corCandidateReturns %>% 
+    group_by(candSignalname) %>%
+    mutate(rbar_dm_is = mean(ret[samptype == "insamp"], na.rm = TRUE)) %>%
+    ungroup() %>%
+    mutate(diff_rbar = abs(rbar_dm_is*sign(rbar_dm_is) - rbar_op)) %>%
+    filter(diff_rbar <= r_tol_cor) %>%
+    filter(samptype == 'insamp') %>%
+    group_by(actSignal, candSignalname) %>% 
+    summarise(rbar_insampMatched = mean(ret)) %>% 
+    ungroup()
+  
+  # Rescale and average over all matched signals for each predictor and event date
+  tempCand = corCandidateReturns %>% 
+    left_join(tempsumCand) %>% 
+    mutate(retOrig = ret,
+           ret = 100*ret/rbar_insampMatched) %>%
+    # mutate(retOrig = ret,
+    #        ret = 100*ret) %>%   
+    group_by(actSignal, eventDate) %>% 
+    summarise(matchRet = mean(ret, na.rm = TRUE),
+              matchRetOrig = mean(retOrig, na.rm = TRUE),
+              nSignals = n()) %>% 
+    ungroup()
+  
+  # Combine with matched signals
+  CorAllRets = czret %>% 
+    left_join(tempCand, by = c('signalname' = 'actSignal', 'eventDate' = 'eventDate')) %>% 
+    # Add matched returns without excluding correlations
+    left_join(allRets %>% transmute(signalname, date, 
+                                    matchRetAlt = matchRet))
+  
+  rm(tempsumCand, tempCand, corCandidateReturns)
+  
+  # Plot returns over time over all categories
+  ReturnPlotsWithDM(dt = CorAllRets %>% 
+                      filter(!is.na(matchRet), Keep == 1) %>% # To exclude unmatched signals
+                      transmute(eventDate, ret, matchRet, matchRetAlt),
+                    basepath = '../Results/Fig_PublicationsVsDataMining',
+                    suffix = paste0('All_DM_Correlation_', cc, '_d_mean_', r_tol_cor),
+                    rollmonths = 60,
+                    colors = colors,
+                    legendlabels = c('Published','Matched data-mined (excl correlated)','Matched data-mined (all)'),
+                    legendpos = c(35,45)/100,
+                    fontsize = 24,
+                    yl = -90, yh = 170
+  )
+  
+  # Plot returns over time by category
+  for (jj in unique(CorAllRets$theory)) {
+    print(jj)
+    
+    ReturnPlotsWithDM(dt = CorAllRets %>% 
+                        filter(!is.na(matchRet), theory == jj, Keep == 1) %>% # To exclude unmatched signals
+                        dplyr::select(eventDate, ret, matchRet, matchRetAlt),
+                      basepath = '../Results/Fig_PublicationsVsDataMining',
+                      suffix = paste0(jj, '_DM_Correlation', cc, '_d_mean_', r_tol_cor),
+                      colors = colors,
+                      legendlabels = c('Published','Matched data-mined (excl correlated)','Matched data-mined (all)'),
+                      legendpos = c(35,45)/100,
+                      fontsize = 24,
+                      yl = -90, yh = 170
+                      
+    ) 
+    
+  }
+  
+}
+
