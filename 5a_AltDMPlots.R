0d79d67a6d087dabf4591e6c920f653bbf24f982
Andrew Y. Chen <65506481+chenandrewy@users.noreply.github.com>
2024-09-12 21:48:51 -0400

    Update 5a_AltDMPlots.R
    added custom alt dm plot for slides (maybe we want to use this in the end)

--- 5a_AltDMPlots.R
+++ 5a_AltDMPlots.R
@@ -11,13 +11,15 @@ library(doParallel)
 
 # settings
 # ncores <- round(detectCores() / 2)
-ncores = 2 # really, ram is the limiting factor...
+ncores <- 2 # really, ram is the limiting factor...
 
 dmcomp <- list()
 dmtic <- list()
-dmcomp$name <- paste0('../Data/Processed/',
-                      globalSettings$dataVersion, 
-                      ' LongShort.RData')
+dmcomp$name <- paste0(
+  "../Data/Processed/",
+  globalSettings$dataVersion,
+  " LongShort.RData"
+)
 dmtic$name <- "../Data/Processed/ticker_Harvey2017JF.RDS"
 
 ## Load Global Data ---------------------------------------------------------------
@@ -67,52 +69,51 @@ save.image("../Data/tmpAltDMPlots.RData")
 # Convenience Load --------------------------------------------------------
 load("../Data/tmpAltDMPlots.RData")
 
-source('0_Environment.R') # in case there are function updates
+source("0_Environment.R") # in case there are function updates
 
 # Functions for making event time returns ---------------------------------
 
 # Shared Plot Setup (plot_one_setting) -------------------------------------------
 
 # shared aesthetic settings
-fontsizeall = 28
-legposall = c(30,15)/100
-ylaball = 'Trailing 5-Year Return (bps pm)'
-linesizeall = 1.5
-plot_one_setting = function(plotdat){
-  
+fontsizeall <- 28
+legposall <- c(30, 15) / 100
+ylaball <- "Trailing 5-Year Return (bps pm)"
+linesizeall <- 1.5
+plot_one_setting <- function(plotdat) {
   # make event time returns for Compustat DM
-  temp = list()
+  temp <- list()
   temp$matched <- SelectDMStrats(dmcomp$insampsum, plotdat$matchset)
-  
+
   print("Making accounting event time returns")
   print("Can take a few minutes...")
   start_time <- Sys.time()
   temp$event_time <- make_DM_event_returns(
-    DMname = dmcomp$name, match_strats = temp$matched, npubmax = plotdat$npubmax, 
+    DMname = dmcomp$name, match_strats = temp$matched, npubmax = plotdat$npubmax,
     czsum = czsum, use_sign_info = plotdat$use_sign_info
   )
   stop_time <- Sys.time()
   stop_time - start_time
-  
+
   plotdat$comp_matched <- temp$matched
   plotdat$comp_event_time <- temp$event_time
-  
+
   # make event time returns for ticker DM
-  temp = list()
+  temp <- list()
   temp$matched <- SelectDMStrats(dmtic$insampsum, plotdat$matchset)
-  
+
   print("Making ticker event time returns")
   start_time <- Sys.time()
   temp$event_time <- make_DM_event_returns(
-    DMname = dmtic$name, match_strats = temp$matched, npubmax = plotdat$npubmax, 
+    DMname = dmtic$name, match_strats = temp$matched, npubmax = plotdat$npubmax,
     czsum = czsum, use_sign_info = plotdat$use_sign_info
   )
   stop_time <- Sys.time()
   stop_time - start_time
-  
+
   plotdat$tic_matched <- temp$matched
   plotdat$tic_event_time <- temp$event_time
-  
+
   # join and reformat for plotting function
   # it wants columns:  (eventDate, ret, matchRet, matchRetAlt)
   # dm_mean returns are already scaled (see above)
@@ -127,7 +128,7 @@ plot_one_setting = function(plotdat){
     select(eventDate, ret, matchRet, matchRetAlt, pubname) %>%
     # keep only rows where both matchrets are observed
     filter(!is.na(matchRet) & !is.na(matchRetAlt))
-  
+
   # plot
   ReturnPlotsWithDM(
     dt = ret_for_plotting,
@@ -141,7 +142,7 @@ plot_one_setting = function(plotdat){
     legendlabels =
       c(
         paste0("Published"),
-        paste0(plotdat$legprefix, " Mining Accounting"), 
+        paste0(plotdat$legprefix, " Mining Accounting"),
         paste0(plotdat$legprefix, " Mining Tickers")
       ),
     legendpos = legposall,
@@ -149,7 +150,6 @@ plot_one_setting = function(plotdat){
     yaxislab = ylaball,
     linesize = linesizeall
   )
-  
 } # end plot_one_setting
 
 # Main Plots ----------------------------------------------------------------
@@ -159,9 +159,9 @@ plot_one_setting = function(plotdat){
 plotdat <- list()
 
 plotdat$name <- "t_min_2"
-plotdat$legprefix = "|t|>2.0"
-plotdat$npubmax = Inf
-plotdat$use_sign_info = TRUE
+plotdat$legprefix <- "|t|>2.0"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
 
 plotdat$matchset <- list(
   # tolerance in levels
@@ -180,33 +180,37 @@ plot_one_setting(plotdat)
 
 ## numbers for intro ----------------------------------------------------------
 
-  # make event time returns for Compustat DM
-  temp = list()
-  temp$matched <- SelectDMStrats(dmcomp$insampsum, plotdat$matchset)
-  
-  print("Making accounting event time returns")
-  print("Can take a few minutes...")
-  start_time <- Sys.time()
-  temp$event_time <- make_DM_event_returns(
-    DMname = dmcomp$name, match_strats = temp$matched, npubmax = plotdat$npubmax, 
-    czsum = czsum, use_sign_info = plotdat$use_sign_info
-  )
-  stop_time <- Sys.time()
-  stop_time - start_time
+# make event time returns for Compustat DM
+temp <- list()
+temp$matched <- SelectDMStrats(dmcomp$insampsum, plotdat$matchset)
 
-  # summarize
-  temp$event_time[ 
-    !is.na(samptype), .(dm_rbar=mean(dm_mean)), by=c('pubname', 'samptype')
-  ] %>% 
-    group_by(samptype) %>% summarize(mean(dm_rbar))
+print("Making accounting event time returns")
+print("Can take a few minutes...")
+start_time <- Sys.time()
+temp$event_time <- make_DM_event_returns(
+  DMname = dmcomp$name, match_strats = temp$matched, npubmax = plotdat$npubmax,
+  czsum = czsum, use_sign_info = plotdat$use_sign_info
+)
+stop_time <- Sys.time()
+stop_time - start_time
 
-  # compare to czret
-  czret %>% 
-    mutate(samptype 
-      = if_else(samptype%in%c('postpub','oos'), 'postsamp', samptype)) %>% 
-    group_by(signalname, samptype) %>% 
-    summarise(rbar = mean(ret_scaled)) %>% 
-    group_by(samptype) %>% summarize(mean(rbar))
+# summarize
+temp$event_time[
+  !is.na(samptype), .(dm_rbar = mean(dm_mean)),
+  by = c("pubname", "samptype")
+] %>%
+  group_by(samptype) %>%
+  summarize(mean(dm_rbar))
+
+# compare to czret
+czret %>%
+  mutate(
+    samptype = if_else(samptype %in% c("postpub", "oos"), "postsamp", samptype)
+  ) %>%
+  group_by(signalname, samptype) %>%
+  summarise(rbar = mean(ret_scaled)) %>%
+  group_by(samptype) %>%
+  summarize(mean(rbar))
 
 
 
@@ -214,9 +218,9 @@ plot_one_setting(plotdat)
 plotdat <- list()
 
 plotdat$name <- "t_rankpct_min_5"
-plotdat$legprefix = "top 5% |t|"
-plotdat$npubmax = Inf
-plotdat$use_sign_info = TRUE
+plotdat$legprefix <- "top 5% |t|"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
 
 plotdat$matchset <- list(
   # tolerance in levels
@@ -239,9 +243,9 @@ plot_one_setting(plotdat)
 plotdat <- list()
 
 plotdat$name <- "t_min_0.5"
-plotdat$legprefix = "|t|>0.5"
-plotdat$npubmax = Inf
-plotdat$use_sign_info = TRUE
+plotdat$legprefix <- "|t|>0.5"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
 
 plotdat$matchset <- list(
   # tolerance in levels
@@ -262,9 +266,9 @@ plot_one_setting(plotdat)
 plotdat <- list()
 
 plotdat$name <- "t_rankpct_min_90"
-plotdat$legprefix = "top 90% |t|"
-plotdat$npubmax = Inf
-plotdat$use_sign_info = TRUE
+plotdat$legprefix <- "top 90% |t|"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
 
 plotdat$matchset <- list(
   # tolerance in levels
@@ -287,9 +291,9 @@ plotdat$matchset <- list(
 plotdat <- list()
 
 plotdat$name <- "t_min_1"
-plotdat$legprefix = "|t|>1.0"
-plotdat$npubmax = Inf
-plotdat$use_sign_info = TRUE
+plotdat$legprefix <- "|t|>1.0"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
 
 plotdat$matchset <- list(
   # tolerance in levels
@@ -312,9 +316,9 @@ plot_one_setting(plotdat)
 plotdat <- list()
 
 plotdat$name <- "t_min_3"
-plotdat$legprefix = "|t|>3.0"
-plotdat$npubmax = Inf
-plotdat$use_sign_info = TRUE
+plotdat$legprefix <- "|t|>3.0"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
 
 plotdat$matchset <- list(
   # tolerance in levels
@@ -338,9 +342,9 @@ plot_one_setting(plotdat)
 plotdat <- list()
 
 plotdat$name <- "t_rankpct_min_75"
-plotdat$legprefix = "top 75% |t|"
-plotdat$npubmax = Inf
-plotdat$use_sign_info = TRUE
+plotdat$legprefix <- "top 75% |t|"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
 
 plotdat$matchset <- list(
   # tolerance in levels
@@ -362,9 +366,9 @@ plot_one_setting(plotdat)
 plotdat <- list()
 
 plotdat$name <- "t_rankpct_min_50"
-plotdat$legprefix = "top 50% |t|"
-plotdat$npubmax = Inf
-plotdat$use_sign_info = TRUE
+plotdat$legprefix <- "top 50% |t|"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
 
 plotdat$matchset <- list(
   # tolerance in levels
@@ -386,9 +390,9 @@ plot_one_setting(plotdat)
 plotdat <- list()
 
 plotdat$name <- "t_rankpct_min_0.5"
-plotdat$legprefix = "top 0.5% |t|"
-plotdat$npubmax = Inf
-plotdat$use_sign_info = TRUE
+plotdat$legprefix <- "top 0.5% |t|"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
 
 plotdat$matchset <- list(
   # tolerance in levels
@@ -406,3 +410,114 @@ plotdat$matchset <- list(
 
 plot_one_setting(plotdat)
 
+# Plot selected lines for slides ---------------------------------------------
+
+# published
+# top 5% mining accounting
+# t>2 mining tickers
+
+plotdat <- list()
+
+plotdat$name <- "custom_slides"
+plotdat$npubmax <- Inf
+plotdat$use_sign_info <- TRUE
+
+
+plotdat$matchset_rankmin <- list(
+  # tolerance in levels
+  t_tol = .1 * Inf,
+  r_tol = .3 * Inf,
+  # tolerance relative to op stat
+  t_reltol = 0.1 * Inf,
+  r_reltol = 0.3 * Inf,
+  # alternative filtering
+  t_min = 0, # minimum screened t-stat
+  t_max = Inf, # maximum screened t-stat
+  t_rankpct_min = 5, # minimum t-stat rank (in pctile)
+  minNumStocks = globalSettings$minNumStocks
+)
+
+plotdat$matchset_t2 <- list(
+  # tolerance in levels
+  t_tol = .1 * Inf,
+  r_tol = .3 * Inf,
+  # tolerance relative to op stat
+  t_reltol = 0.1 * Inf,
+  r_reltol = 0.3 * Inf,
+  # alternative filtering
+  t_min = 2, # minimum screened t-stat
+  t_max = Inf, # maximum screened t-stat
+  t_rankpct_min = 100, # top x% of data mined t-stats, 100% for off
+  minNumStocks = globalSettings$minNumStocks
+)
+
+
+# make event time returns for Compustat DM
+temp <- list()
+temp$matched <- SelectDMStrats(dmcomp$insampsum, plotdat$matchset_rankmin)
+
+print("Making accounting event time returns")
+print("Can take a few minutes...")
+start_time <- Sys.time()
+temp$event_time <- make_DM_event_returns(
+  DMname = dmcomp$name, match_strats = temp$matched, npubmax = plotdat$npubmax,
+  czsum = czsum, use_sign_info = plotdat$use_sign_info
+)
+stop_time <- Sys.time()
+stop_time - start_time
+
+plotdat$comp_matched <- temp$matched
+plotdat$comp_event_time <- temp$event_time
+
+# make event time returns for ticker DM
+temp <- list()
+temp$matched <- SelectDMStrats(dmtic$insampsum, plotdat$matchset_t2)
+
+print("Making ticker event time returns")
+start_time <- Sys.time()
+temp$event_time <- make_DM_event_returns(
+  DMname = dmtic$name, match_strats = temp$matched, npubmax = plotdat$npubmax,
+  czsum = czsum, use_sign_info = plotdat$use_sign_info
+)
+stop_time <- Sys.time()
+stop_time - start_time
+
+plotdat$tic_matched <- temp$matched
+plotdat$tic_event_time <- temp$event_time
+
+# join and reformat for plotting function
+# it wants columns:  (eventDate, ret, matchRet, matchRetAlt)
+# dm_mean returns are already scaled (see above)
+ret_for_plotting <- czret %>%
+  transmute(pubname = signalname, eventDate, ret = ret_scaled) %>%
+  left_join(
+    plotdat$comp_event_time %>% transmute(pubname, eventDate, matchRet = dm_mean)
+  ) %>%
+  left_join(
+    plotdat$tic_event_time %>% transmute(pubname, eventDate, matchRetAlt = dm_mean)
+  ) %>%
+  select(eventDate, ret, matchRet, matchRetAlt, pubname) %>%
+  # keep only rows where both matchrets are observed
+  filter(!is.na(matchRet) & !is.na(matchRetAlt))
+
+# plot
+ReturnPlotsWithDM(
+  dt = ret_for_plotting,
+  basepath = "../Results/Fig_AltDM",
+  suffix = plotdat$name,
+  rollmonths = 60,
+  colors = colors,
+  labelmatch = FALSE,
+  yl = -50,
+  yh = 140,
+  legendlabels =
+    c(
+      paste0("Published"),
+      paste0("top 5% |t| Mining Accounting"),
+      paste0("|t|>2.0 Mining Tickers")
+    ),
+  legendpos = legposall,
+  fontsize = fontsizeall,
+  yaxislab = ylaball,
+  linesize = linesizeall
+)
