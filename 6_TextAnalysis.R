304cf94a2783bcfe32303f46a5583de4768a1be8
Tom Zimmermann <32535583+tomz23@users.noreply.github.com>
2024-09-16 10:19:13 +0200

    Very light editing of text analysis files (more editing needed)

--- 6_TextAnalysis.R
+++ 6_TextAnalysis.R
@@ -1,9 +1,7 @@
 # wrapper for text analysis scripts 
 
-# Setup -------------------------------------------------------------------
-source('0_Environment.R')
-
 # Run stuff ----------------------------------------------------------------
 source('6a_TextCleaning.R')
 source('6b_TextTables.R')
 source('6c_DecayVsWordcountPlot.R')
+source('6d_DecayVsModelcountPlot.R')
