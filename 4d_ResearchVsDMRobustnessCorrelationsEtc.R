9fc26d610804f07c9d4b97ce75792038cecbf0f2
Andrew Y. Chen <65506481+chenandrewy@users.noreply.github.com>
2025-02-28 12:23:50 -0500

    Update 4d_ResearchVsDMRobustnessCorrelationsEtc.R
    remove png

--- 4d_ResearchVsDMRobustnessCorrelationsEtc.R
+++ 4d_ResearchVsDMRobustnessCorrelationsEtc.R
@@ -101,44 +101,6 @@ for (jj in unique(allRets$theory)) {
 }
 
 
-
-source('0_Environment.R')
-# Plot re-scaled returns over time by category, hide
-for (jj in unique(allRets$theory)) {
-  print(jj)
-  
-  ReturnPlotsWithDM(dt = allRets %>% 
-                      filter(!is.na(matchRet), theory == jj, Keep == 1) %>% # To exclude unmatched signals
-                      dplyr::select(eventDate, ret, matchRet),
-                    basepath = '../Results/Extra/Fig_PublicationsVsDataMining',
-                    suffix = paste0(jj, '_DM_hide'),
-                    colors = colors,
-                    xl = global_xl,
-                    xh = global_xh,
-                    yl = -60, yh = 170, fig.width = 18, fontsize = 28
-                    , hideoos = TRUE
-                    , filetype = '.png') 
-  
-}
-
-# Plot re-scaled returns over time by category
-for (jj in unique(allRets$theory)) {
-  print(jj)
-  
-  ReturnPlotsWithDM(dt = allRets %>% 
-                      filter(!is.na(matchRet), theory == jj, Keep == 1) %>% # To exclude unmatched signals
-                      dplyr::select(eventDate, ret, matchRet),
-                    basepath = '../Results/Extra/Fig_PublicationsVsDataMining',
-                    suffix = paste0(jj, '_DM'),
-                    colors = colors,
-                    xl = global_xl,
-                    xh = global_xh,
-                    yl = -60, yh = 170, fig.width = 18, fontsize = 28
-                    , hideoos = FALSE
-                    , filetype = '.png') 
-  
-}
-
 # Excluding high correlations --------------------------------------------------
 allRhos = readRDS('../Results/PairwiseCorrelationsActualAndMatches.RDS')
 
